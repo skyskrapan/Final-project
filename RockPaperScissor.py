057333cdaec528207dbdd1b675e06f26ac9d535a
Hanna Kampf <125279264+skyskrapan@users.noreply.github.com>
2023-03-28 09:43:18 +0200

    Försöker lösa att påse vinner över sten vilket inte funkar nu, alla andra drag funkar.

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,5 +1,9 @@
 import random
 
+sten = 1
+sax = 2
+påse = 3
+
 sten > sax
 sax > påse
 påse > sten
@@ -7,11 +11,22 @@ påse > sten
 olika_drag = ["sten", "sax", "påse"]
 
 drag1 = input()
-drag2 = print(random.choice(olika_drag))
+drag2 = str(random.choice(olika_drag))
+print(drag2)
+
+if drag1 == påse and drag2 == sten:
+    print("Du vann!")
+        
+else:
+    pass
+
 if drag1 > drag2:
     print("Du vann")
+          
 elif drag1 == drag2:
     print("Det blev lika")
+    
 else:
     print("Du förlora")
     
+
