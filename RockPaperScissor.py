f6e0cb5595a8db4634ae4a599e4d91a46fe3cbf6
Hanna Kampf <125279264+skyskrapan@users.noreply.github.com>
2023-04-17 15:29:11 +0200

    Gjort den spelbar, alltså alla drag funkar

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,26 +1,28 @@
 import random
 
-sten = 1
-sax = 2
-påse = 3
-
-sten > sax
-sax > påse
-påse > sten
-
 olika_drag = ["sten", "sax", "påse"]
 
 drag1 = input("Skriv in vilket drag du vill göra: ")
 drag2 = str(random.choice(olika_drag))
 print("Motståndaren valde:", drag2)
 
-if drag1 > drag2:
-    print("Du vann")
-          
-elif drag1 == drag2:
-    print("Det blev lika")
+if drag1 == drag2:
+    print("Det blev lika!")
     
-else:
-    print("Du förlora")
-    
+elif drag1 == "sten":
+    if drag2 == "påse":
+        print("Du förlora!")
+    else:
+         print("Du vann!")
 
+elif drag1 == "påse":
+    if drag2 == "sten":
+        print("Du vann!")
+    else:
+        print("Du förlora!")
+
+elif drag1 == "sax":
+    if drag2 == "påse":
+        print("Du vann!")
+    else:
+        print("Du förlora!")
