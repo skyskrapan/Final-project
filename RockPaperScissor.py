ff990f950fe20377b40dccad8ffb4b97dbe25557
Hanna Kampf <125279264+skyskrapan@users.noreply.github.com>
2023-04-18 09:52:42 +0200

    försöker lösa att man inte kan skriva fel på draget och kunna fortsätta

--- RockPaperScissor.py
+++ RockPaperScissor.py
@@ -1,28 +1,36 @@
 import random
 
 olika_drag = ["sten", "sax", "påse"]
+while True:
+    drag1 = input("Skriv in vilket drag du vill göra: ")
 
-drag1 = input("Skriv in vilket drag du vill göra: ")
-drag2 = str(random.choice(olika_drag))
-print("Motståndaren valde:", drag2)
-
-if drag1 == drag2:
-    print("Det blev lika!")
+    if drag1 != "sten":
+        print("Det draget finns inte, använd sten, sax eller påse :)")
+        break
+    else:
+        continue
     
-elif drag1 == "sten":
-    if drag2 == "påse":
-        print("Du förlora!")
-    else:
-         print("Du vann!")
+    drag2 = str(random.choice(olika_drag))
+    print("Motståndaren valde:", drag2)
 
-elif drag1 == "påse":
-    if drag2 == "sten":
-        print("Du vann!")
-    else:
-        print("Du förlora!")
+    if drag1 == drag2:
+        print("Det blev lika!")
+    
+    elif drag1 == "sten":
+        if drag2 == "påse":
+            print("Du förlora!")
+        else:
+            print("Du vann!")
 
-elif drag1 == "sax":
-    if drag2 == "påse":
-        print("Du vann!")
-    else:
-        print("Du förlora!")
+    elif drag1 == "påse":
+        if drag2 == "sten":
+            print("Du vann!")
+        else:
+            print("Du förlora!")
+
+    elif drag1 == "sax":
+        if drag2 == "påse":
+            print("Du vann!")
+        else:
+            print("Du förlora!")
+    break
